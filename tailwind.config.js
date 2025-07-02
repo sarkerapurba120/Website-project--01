6d984852fcf461484396198143f1d2e7c0d30ff0
Apurba Sarker (অপূর্ব). <206647056+sarkerapurba120@users.noreply.github.com>
2025-07-02 13:54:28 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
